e62dd5b0828f639ec3b5d3d2a2d93f882c5cefef
tomlee55 <s3262421@student.rmit.edu.au>
2012-08-28 14:52:09 +1000

    add query year and variety

--- search.php
+++ search.php
@@ -14,21 +14,49 @@
     if(!$conn) exit;
     mysql_select_db(DB_NAME, $conn);
 
-	  $query = 'SELECT * FROM region ORDER BY region_name';
+	  // get all region
+	  $queryRegion = 'SELECT * FROM region ORDER BY region_name';
 	  $idRegion = array();
 	  $regionname = array();
-	  $result = @ mysql_query ($query, $conn);
+	  $result = @ mysql_query ($queryRegion, $conn);
+	  $i = 0;
+	  while($row = @ mysql_fetch_row($result)){
+	  $idRegion[$i] = $row[0];
+	  $regionname[$i] = $row[1];
+	  $i++;
+	  }
+
+	  $smarty->assign('idRegion',$idRegion);
+	  $smarty->assign('regionname',$regionname);
+	  
+	  
+	  	//get all years
+	  $queryYear = 'SELECT DISTINCT year FROM wine ORDER BY year';
+	  $year = array();
+	  $result = @ mysql_query ($queryYear, $conn);
+	  $i = 0;
+	  while($row = @ mysql_fetch_row($result)){
+	  $year[$i] = $row[0];
+	  $i++;
+	  }
+
+	  $smarty->assign('year',$year);
+			
+	  //ger all grapevariety
+	  $queryGrape = 'SELECT * FROM grape_variety ORDER BY variety';
+	  $idGrape = array();
+	  $grapevariety = array();
+	  $result = @ mysql_query ($queryGrape, $conn);
 	  $i=0;
       while ($row = @ mysql_fetch_row($result)){
-	       $idRegion[$i] = $row[0];
-	       $regionname[$i] = $row[1];
+	       $idGrape[$i] = $row[0];
+	       $grapevariety[$i] = $row[1];
 		   $i++;
 	}
+	$smarty->assign('idGrape',$idGrape);
+	$smarty->assign('grapevariety',$grapevariety);
 	
-	$smarty->assign('idRegion',$idRegion);
-	$smarty->assign('regionname',$regionname);
-			
-	  
+
 	  
 	 $smarty->display('search.tpl');
 	 
